2dd67bad7419dc2ae1d86b0aee0195ac4e97affe
Wangjunjie991116 <2638241171@qq.com>
2025-11-07 03:30:25 +0800

    feat: add vue-i18n

--- pages/demo1/index.vue
+++ pages/demo1/index.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 <template>
   <div class="demo-container">
-    <h1 class="title">demo1</h1>
+    <h1 class="title">demo1___i18n___:{{ $t("key") }}</h1>
     <VanButton type="success" @click="showNotify('notify')">button</VanButton>
   </div>
 </template>
@@ -9,13 +9,13 @@
 <style lang="less" scoped>
 .demo-container {
   padding: 20px;
-  
+
   .title {
     color: #333;
     font-size: 24px;
     margin-bottom: 16px;
     background-color: pink;
-    
+
     &:hover {
       color: #1890ff;
     }
