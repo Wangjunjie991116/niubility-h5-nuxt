b20bad4a53819420b9a15b0e2630d22608287bd4
Wangjunjie991116 <2638241171@qq.com>
2025-11-07 01:33:28 +0800

    feat: init config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,12 @@
-import { defineNuxtConfig } from "nuxt/config";
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   modules: ["@nuxt/fonts", "@nuxt/eslint", "@nuxt/scripts"],
+  // routeRules: {
+  //   "/ddr_status/**": { ssr: true },
+  //   "/blog/**": { swr: true },
+  //   "/static/**": { static: true },
+  //   "/api/**": { headers: { "x-robots-tag": "noindex" } },
+  // },
 });
